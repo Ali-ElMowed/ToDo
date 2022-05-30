04961b9ff4e0f5f1edca5299dc55210b7c55ffc1
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-30 13:38:36 +0300

    e Added on click function for creare Added cancel button

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let create=document.getElementById("add-todo");
+let createPopup=document.getElementById("pop-up");
+
+
+$(create).click(function(){
+    $(createPopup).css("display","block");
+}
+);
