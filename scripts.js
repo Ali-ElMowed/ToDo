46d158d157542c809b93377b924e8c27798d89f6
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-06-01 13:53:26 +0300

    Added some ids and comments

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-let create=document.getElementById("add-todo");
-let createPopup=document.getElementById("pop-up");
+let create = document.getElementById("add-todo");
+let createPopup = document.getElementById("pop-up");
+let cancel = document.getElementById("cancel");
+let done= document.getElementById("done");
+let edit = document.getElementById("edit");
+let deleteTodo = document.getElementById("delete");
+let 
+$(create).click(function () {
+    $(createPopup).css("display", "block");
+});
+
+$(cancel).click(function () {
+    $(createPopup).css("display", "none");
+});
 
 
-$(create).click(function(){
-    $(createPopup).css("display","block");
-}
-);
